e89c711da8a78fa1576ab2bbf9795a271093d0eb
efrainmpp1 <efrainmpp@hotmail.com>
2023-10-24 08:14:32 -0300

    put to project pattern

--- backend/challenge_api/models.py
+++ backend/challenge_api/models.py
@@ -4,7 +4,7 @@ class TransactionType(models.Model):
     id = models.PositiveSmallIntegerField(primary_key=True)
     description = models.CharField(max_length=255)
     nature = models.CharField(max_length=10, choices=[('Entrada', 'Entrada'), ('Saída', 'Saída')])
-    sinal = models.CharField(max_length=1, choices=[('+', '+'), ('-', '-')])
+    signal = models.CharField(max_length=1, choices=[('+', '+'), ('-', '-')])
 
     def __str__(self):
         return self.description
