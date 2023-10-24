70e4d93d903ebdff89d4cc764f66e38b3b7a126c
efrainmpp1 <efrainmpp@hotmail.com>
2023-10-24 11:42:09 -0300

    create transaction table

--- backend/challenge_api/models.py
+++ backend/challenge_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from dateutil.parser import parse
 
 class TransactionType(models.Model):
     id = models.PositiveSmallIntegerField(primary_key=True)
@@ -8,3 +9,20 @@ class TransactionType(models.Model):
 
     def __str__(self):
         return self.description
+    
+class Transaction(models.Model):
+    id = models.PositiveIntegerField(primary_key=True,auto_created=True)
+    transaction_type_id = models.ForeignKey(TransactionType, on_delete=models.DO_NOTHING)
+    date = models.DateTimeField()
+    product_description = models.CharField(max_length=30)
+    amount_in_cents = models.IntegerField()
+    seller_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Transaction of {self.amount_in_cents} cents"
+
+    def set_date(self, data_string):
+        self.date = parse(data_string)
+    
+    def get_amount_in_reais(self):
+        return self.amount_in_cents / 100.0
