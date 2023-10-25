03131d5a0197d26aec65311949a75a4c85848b1a
efrainmpp1 <efrainmpp@hotmail.com>
2023-10-25 04:54:46 -0300

    transform txt content into list of objects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,27 @@ import "./App.css";
 
 function App() {
   const handleFileUpload = (fileContent) => {
-    console.log(fileContent);
+    // Divide o conteúdo do arquivo em linhas
+    const lines = fileContent.split("\n");
+
+    // Cria uma lista de objetos com as chaves especificadas, ignorando as linhas em branco
+    const data = lines
+      .map((line) => ({
+        transaction_type_id: parseInt(line[0]),
+        date: line.slice(1, 26).trim(),
+        product_description: line.slice(26, 56).trim(),
+        amount_in_cents: parseInt(line.slice(56, 66).trim()),
+        seller_name: line.slice(66, 86).trim(),
+      }))
+      .filter(
+        (item) =>
+          item.date ||
+          item.product_description ||
+          item.amount_in_cents ||
+          item.seller_name
+      );
+
+    console.log(data);
   };
 
   return (
