fc31d0a37edfe9a53e5de0c2ddef520f8e3f1151
efrainmpp1 <efrainmpp@hotmail.com>
2023-10-26 13:06:13 -0300

    add help to see severity options

--- frontend/src/components/Notification/index.jsx
+++ frontend/src/components/Notification/index.jsx
@@ -7,6 +7,7 @@ const Alert = forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
+// severity options: 'info' || 'error' || 'success' || 'warning'
 export default function Notification({
   open,
   setOpen = () => {},
