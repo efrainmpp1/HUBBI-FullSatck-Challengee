2ad2b481a9f744a47a3a2abfe6afe7ce8fd75e5f
efrainmpp1 <efrainmpp@hotmail.com>
2023-10-26 12:40:13 -0300

    add notification for error handling

--- frontend/src/screen/MassiveTransactionRegistration/index.jsx
+++ frontend/src/screen/MassiveTransactionRegistration/index.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import FileUploader from "../../components/Upload";
 import Button from "../../components/Button";
+import Notification from "../../components/Notification";
 import { massiveRegisterTransactions } from "../../services/transaction";
 import "./style.css";
 
 function MassiveTransactionRegistration() {
   const [transactions, setTransactions] = useState([]);
+  const [openNotification, setOpenNotification] = useState(false);
+  const [notificationStatus, setNotificationStatus] = useState({
+    severity: "info",
+    message: null,
+  });
 
   const handleFileUpload = (fileContent) => {
     // Divide o conteúdo do arquivo em linhas
@@ -33,16 +39,28 @@ function MassiveTransactionRegistration() {
 
   const handleSubmit = async (data) => {
     try {
-      await massiveRegisterTransactions(data);
+      const response = await massiveRegisterTransactions(data);
+      if (response.status == 201) {
+        setNotificationStatus({
+          severity: "success",
+          message: "As transações foram Cadastradas com Sucesso",
+        });
+      }
+      setOpenNotification(true);
       setTransactions([]);
     } catch (error) {
-      console.log(error);
+      setNotificationStatus({
+        severity: "error",
+        message: "Houve um erro ao tentar cadastrar as transações",
+      });
+      setOpenNotification(true);
+      setTransactions([]);
     }
   };
 
   return (
     <>
-      <h1>Teste</h1>
+      <h1>Cadastro Massivo de Transações</h1>
       <div>
         <FileUploader onFileUpload={handleFileUpload} />
         {transactions.length > 0 ? (
@@ -54,6 +72,13 @@ function MassiveTransactionRegistration() {
           <></>
         )}
       </div>
+      <Notification
+        open={openNotification}
+        setOpen={setOpenNotification}
+        severity={notificationStatus.severity}
+      >
+        {notificationStatus.message}
+      </Notification>
     </>
   );
 }
