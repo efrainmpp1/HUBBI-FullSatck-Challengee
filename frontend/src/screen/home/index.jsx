a5f0af35302a9bcd6d33122701cf6ba6af0e7185
efrainmpp1 <efrainmpp@hotmail.com>
2023-10-26 11:21:34 -0300

    add style css to component

--- frontend/src/screen/home/index.jsx
+++ frontend/src/screen/home/index.jsx
@@ -1,5 +1,6 @@
 import ActionAreaCard from "../../components/ActionAreaCard";
 import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 function Home() {
   const navigate = useNavigate();
@@ -7,20 +8,24 @@ function Home() {
   return (
     <>
       <h1>Hubbi Challenge</h1>
-      <div>
-        <ActionAreaCard
-          tittle="Cadastrar Transações"
-          onClick={() => navigate("/massive_transaction_registration")}
-        >
-          Essa sessão é usada para Cadastrar Transações no Banco de Dados
-          massivamente
-        </ActionAreaCard>
-        <ActionAreaCard
-          tittle="Lista de Transações"
-          onClick={() => navigate("/transaction_history")}
-        >
-          Essa sessão é usada para Listar as Transações já realizadas
-        </ActionAreaCard>
+      <div className="div-card">
+        <div className="card">
+          <ActionAreaCard
+            tittle="Cadastrar Transações"
+            onClick={() => navigate("/massive_transaction_registration")}
+          >
+            Essa sessão é usada para Cadastrar Transações no Banco de Dados
+            massivamente
+          </ActionAreaCard>
+        </div>
+        <div className="card">
+          <ActionAreaCard
+            tittle="Lista de Transações"
+            onClick={() => navigate("/transaction_history")}
+          >
+            Essa sessão é usada para Listar as Transações já realizadas
+          </ActionAreaCard>
+        </div>
       </div>
     </>
   );
